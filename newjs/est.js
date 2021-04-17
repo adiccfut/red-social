9c0d146faf64b90919d2672a7917786f1e359647
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-04-17 14:53:15 -0300

    Add files via upload

--- newjs/est.js
+++ newjs/est.js
@@ -1,8 +1,8 @@
 
 EstEspA = [];
 	var i3 = -1;
-for (var i = 0; i <= 2; i++) {
-	var EstEsp = ["<a class='waves-effect waves-light btnoutlinelight outline ahref' href='"+Est[i3=i3+1]+"' style='background-color: #ffffff00;'><b>Estadisticas</b></a> "];
+for (var i = 0; i <= 30; i++) {
+	let EstEsp = ["<a class='waves-effect waves-light btnoutlinelight outline ahref'  href='"+Est[i3=i3+1]+"' style='background-color: #ffffff00;'>Estadisticas</a> "];
 	EstEspA.push(EstEsp);
 }
 	console.log(EstEspA);
